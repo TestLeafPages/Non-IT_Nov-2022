75af60ab115075b3735989d266aca9d61afb4c56
TestLeafPages <earth@testleaf.com>
2022-12-03 13:47:07 +0530

    Add files via upload

---
+++ week2/day1/LaunchBrowser.java
@@ -0,0 +1,23 @@
+package week2day1;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class LaunchBrowser {
+
+	public static void main(String[] args) {
+		
+				
+		//Driver
+		//browser		
+		//url
+		
+		//set a path for driver
+		System.setProperty("webdriver.chrome.driver", "./driver/chromedriver.exe");
+		ChromeDriver driver = new ChromeDriver();
+		
+		
+		
+
+	}
+
+}
